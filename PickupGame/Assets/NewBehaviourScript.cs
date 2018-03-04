d3c25bfa76f7a92a512ce30a79281f0906018704
RickW8 <32253568+RickW8@users.noreply.github.com>
2018-03-04 17:31:51 -0500

    VR Cardboard Game
    123

---
+++ PickupGame/Assets/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public Text countText;
+	public tumbleOut _tumbleOut;
+	public int count;
+	// Use this for initialization
+	public void Start () {
+		countText = GetComponent<Text> () as Text;
+		countText.text = "Obj left: 5";
+		count = 5;
+	}
+
+	public void doIt () {
+		count--;
+		countText.text = "Obj left: "+count.ToString();
+        if (count == 0){
+            countText.text = "Congrats. You win!";
+        }
+	}
+}
---
+++ PickupGame/Assets/tumbleOut.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tumbleOut : MonoBehaviour {
+	public GameObject a;
+
+	public NewBehaviourScript _NewBehaviourScript;
+    public Shader shader1, shader2, shader3, shader4, shader5;
+    void Start()
+	{
+		gameObject.transform.position = new Vector3(
+            GetRandCoord(), Random.Range(-3, 2), GetRandCoord());
+
+
+
+    }
+
+    public void RandomlyTeleport()
+    {
+        gameObject.transform.position = new Vector3(10, 10, 10);
+
+		a.GetComponent<NewBehaviourScript>().doIt();
+
+    }
+
+    private float GetRandCoord()
+    {
+        var coord = Random.Range(-6, 6);
+        while(coord> -2 && coord < 2)
+        {
+            coord = Random.Range(-6, 6);
+        }
+        return coord;
+        
+    }
+}
+    
+/*public void Update()
+    {
+	if(count==1){
+		countText.text = count.ToString();
+    }
+}
+*/
